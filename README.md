4e526437115dcbe0323c94eacaa76be91ba619eb
leomet07 <leomet07@gmail.com>
2022-12-17 21:32:56 -0500

    basic demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Queenslink model demo
+
+Credit for the NeilArmstrong model:
+
+The NeilArmstrong.glb model file located at /public/models/NeilArmstrong.glb is from the [https://github.com/google/model-viewer/tree/master/packages/shared-assets](https://github.com/google/model-viewer/tree/master/packages/shared-assets) repository.
---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+</script>
+
+<main>
+	<h1>Queenslink demo</h1>
+	<p>
+		Feel free to zoom in and pan around. (scroll to zoom in, click and drag
+		to pan)
+	</p>
+	<div id="model_viewer_parent">
+		<model-viewer
+			id="model_viewer"
+			alt="A basic old render of QueensLink"
+			src="/models/rbb.glb"
+			shadow-intensity="1"
+			camera-controls
+			touch-action="pan-y"
+			auto-rotate
+		/>
+		<!-- <model-viewer
+			id="model_viewer"
+			alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
+			src="/NeilArmstrong.glb"
+			shadow-intensity="1"
+			camera-controls
+			touch-action="pan-y"
+		/> -->
+	</div>
+</main>
+
+<style>
+	main {
+		width: 100vw;
+		min-height: 100vh;
+	}
+	#model_viewer_parent {
+		width: 100vw;
+		height: 95vh;
+	}
+	#model_viewer {
+		width: 100%;
+		height: 100%;
+	}
+	h1 {
+		margin-top: 10px;
+		text-align: center;
+	}
+	main p {
+		text-align: center;
+	}
+</style>
